9b0c28299664b139334e0610430d0f41575846f1
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-29 08:14:29 +0300

    update

--- java/classes/TaskTwo.java
+++ java/classes/TaskTwo.java
@@ -51,20 +51,3 @@ public class TaskTwo {
     }
 }
 
-class User {
-    private String name;
-    private int age;
-
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-}
